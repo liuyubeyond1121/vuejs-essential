00a292fc5f426299c51f9f8b01fba61f76e3a30f
liuyubeyond1121 <liuyubeyond1121@gmail.com>
2018-08-11 11:06:26 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
+import './directives'
+import './components'
+// 引入 store/index.js 的默认值
+import store from './store'
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+
+// 使用插件
+Vue.use(VueSweetalert2)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
+  // 注入 store
+  store,
   components: { App },
   template: '<App/>'
 })
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 引入 ./routes.js 的默认值
+import routes from './routes'
+
+Vue.use(Router)
+
+// 这里删除了原来的 routes 常量
+
+const router =  new Router({
+  mode: 'history',
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = router.app.$options.store.state.auth
+
+  if (auth && to.path.indexOf('/auth/') !== -1) {
+    next('/')
+  } else {
+    next()
+  }
+})
+
+export default router
