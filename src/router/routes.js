acb47082cbf0ee92379c07f8766ed9ca91ec378e
liuyubeyond1121 <liuyubeyond1121@gmail.com>
2018-08-14 09:35:46 +0800

    添加个人专栏

--- src/router/routes.js
+++ src/router/routes.js
@@ -50,14 +50,25 @@ export default [
       meta: { auth: true }
     },
     {
-      path: '/articles/:articleId/content',
-      name: 'Content',
-      component: () => import('@/views/articles/Content.vue')
-    },
-    {
       path: '/articles/:articleId/edit',
       name: 'Edit',
       component: () => import('@/views/articles/Create'),
       meta: { auth: true }
+    },
+    {
+      path: '/:user',
+      component: () => import('@/views/articles/Column'),
+      children: [
+        {
+          path: '',
+          name: 'Column',
+          component: () => import('@/views/articles/List.vue')
+        },
+        {
+          path: '/articles/:articleId/content',
+          name: 'Content',
+          component: () => import('@/views/articles/Content.vue')
+        }
+      ]
     },
   ]
